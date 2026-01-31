4ce62be93e896966213a93fd0ef81bd7f48d9795
tnhilanne <nikolas.developer@outlook.com>
2026-01-31 12:49:33 +0200

    reafactored app

--- example-backend/models/note.js
+++ example-backend/models/note.js
@@ -1,26 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-// Use the connection string from environment variable for better security
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-
-  .then(result => {
-    console.log('connected to MongoDB', result.connection.name)
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-// Define the schema for a note, validating content field
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -32,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
